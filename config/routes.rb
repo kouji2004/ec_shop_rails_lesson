5ebea41773dfd873f9b2508195450f9c9b8c147d
kouji2004 <mrtaxi48@gmail.com>
2022-12-19 00:12:38 +0000

    ジャンル機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :items, only: [:new,:create]
-
+    resources :genres, only: [:index,:create]
   end
 
   scope module: :public do
