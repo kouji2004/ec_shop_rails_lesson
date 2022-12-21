8f82d95998b758159f54d1b5782a91add44bba73
kouji2004 <mrtaxi48@gmail.com>
2022-12-21 10:14:32 +0000

    商品をカート内に入れることができる

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,4 +3,22 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
   end
+
+  def create
+       @cart_item = CartItem.new(cart_item_params)
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.item_count += params[:cart_item][:item_count].to_i
+      @cart_item.update(item_count: cart_item.item_count)
+      redirect_to cart_items_path
+    else @cart_item.save
+      redirect_to cart_items_path
+    end
+  end
+
+
+private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :customer_id, :item_count)
+  end
 end
