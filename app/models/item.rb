6e6a3e645de0d2a304caecfb31c3780a17e37840
kouji2004 <mrtaxi48@gmail.com>
2022-12-21 11:08:15 +0000

    カート内商品の合計を出す

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :item_image
   has_many :cart_items
 
-   def get_item_image(width, height)
+  def get_item_image(width, height)
     unless item_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
@@ -11,5 +11,12 @@ class Item < ApplicationRecord
     item_image.variant(resize_to_limit: [width, height]).processed
   end
 
+ def with_tax_price
+    (price * 1.1).floor
+ end
+
+  def subtotal
+      item.with_tax_price * amount
+  end
 
 end
