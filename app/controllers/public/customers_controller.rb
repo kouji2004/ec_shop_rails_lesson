86634cc58eabd2923b86d58cfb37feca60cbae34
kouji2004 <mrtaxi48@gmail.com>
2022-12-21 13:05:00 +0000

    注文情報確認画面の作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,7 @@
 class Public::CustomersController < ApplicationController
+
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kata, :first_name_kata, :post_code, :address, :phone_number, :email)
+  end
 end
