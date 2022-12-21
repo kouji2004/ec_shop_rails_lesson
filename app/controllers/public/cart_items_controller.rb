8dc34760faf35fbbb8b1b2c44d6cf35f0c656324
kouji2004 <mrtaxi48@gmail.com>
2022-12-21 11:11:48 +0000

    カート内の商品を削除する

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -22,6 +22,14 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
+def destroy
+  cart_item = CartItem.find(params[:id])
+  cart_item.destroy
+  @cart_items = CartItem.all
+  redirect_to cart_items_path
+end
+
+
 def all_destroy
   @cart_items = CartItem.all
   @cart_items.destroy_all
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   scope module: :public do
     resources :items ,only: [:index,:show]
-      resources :cart_items ,only: [:index,:create,:update] do
+      resources :cart_items ,only: [:index,:create,:update,:destroy] do
         collection do
           delete 'all_destroy'
         end
